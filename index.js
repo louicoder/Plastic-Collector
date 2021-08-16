8c47260dc758d7d6f220c737a5a444fa8db5c9db
louicoder <musanje2010@gmail.com>
2021-08-16 17:14:59 +0300

    Add app.yaml for app engine instance

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require('dotenv').config();
 
-app.listen(process.env.PORT, () => {
-  console.log(`:::: SERVER RUNNINg ON PORT: ${process.env.PORT}`);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`:::: SERVER RUNNINg ON PORT: ${process.env.PORT} ::::`);
 });
